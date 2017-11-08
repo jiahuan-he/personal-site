e9930c3ee31937ba1ae5222a52314b8e7c7f88dd
Jiahuan He <jiahuan_he@outlook.com>
2017-11-08 13:44:40 -0600

    Remove imgs

--- scripts/script.js
+++ scripts/script.js
@@ -3,11 +3,11 @@ const cactusImg = document.getElementById("cactus-img");
 const hoverRedUnderline = document.getElementsByClassName("hover-red-underline");
 
 const changeToColoredImage = () => {
-    cactusImg.src = "img/cactus-color.png";
+    cactusImg.src = "img/watch.png";
 };
 
 const changeToUncoloredImage = () => {
-    cactusImg.src = "img/cactus-bnw.png";
+    cactusImg.src = "img/blink.png";
 };
 
 header.onmouseover = changeToColoredImage;
